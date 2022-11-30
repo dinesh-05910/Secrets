494a5bca4eb9f7c794e5577909f2701c0be50853
Chinta Dinesh <dineshchinta2001@gmail.com>
2022-11-30 12:34:10 +0530

    Commented localhost:3000 Callback URL

--- app.js
+++ app.js
@@ -65,7 +65,8 @@ passport.deserializeUser(function(id, done){
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets" || "https://node-secrets.onrender.com/auth/google/secrets",
+    //callbackURL: "http://localhost:3000/auth/google/secrets" 
+    callbackURL: "https://node-secrets.onrender.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
